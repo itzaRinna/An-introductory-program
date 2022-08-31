8af5c864ae76ef59f794125f9099e39bb809a80a
elloworldtom <111746841+elloworldtom@users.noreply.github.com>
2022-08-31 16:02:03 +0700

    Add files via upload
    Update!

--- WELCOME_To_CyZero.py
+++ WELCOME_To_CyZero.py
@@ -1,8 +1,8 @@
 import sys
 from time import sleep
 
-ANIMATE_DURATION = 0.0095
-SLOW_ANIMATE_DURATION = 0.095
+ANIMATE_DURATION = 0.009
+SLOW_ANIMATE_DURATION = 0.075
 
 
 def txt_animate(text):
@@ -75,7 +75,7 @@ def main():
     print(transition)
 
     slow_txt_animate(
-        "Great\nI have already known your name!\nWhat is your nickname?\nIt's okay if you don't have one!\n")
+        "Great\nI have already known your name!Which is {}\nWhat is your nickname?\nIt's okay if you don't have one!\n".format(qst_1))
 
     qst_2 = input("Just leave it blank and Enter__")
     if not qst_2:
@@ -84,11 +84,13 @@ def main():
     else:
         slow_txt_animate("I shall call you {}!Cos we're now friend!\n".format(qst_2))
 
-    slow_txt_animate("So {},it would be rude to ask about this...\nBut what is your date of birth?\nAgain if you don't want to answer\n".format(qst_2))
+    slow_txt_animate("So {},it would be rude to ask about this...\nBut what is your date of birth?\nYet again if you don't want to answer\n".format(qst_2))
 
     qst_3 = input("Just leave it blank and Enter__")
     if not qst_3:
-        slow_txt_animate("Sorry for being informal......")
+        slow_txt_animate("Terribly sorry...my dear {},for being informal......".format(qst_2))
+    else:
+        slow_txt_animate("Oh great!I will definitely come to your birthday")
 
 
 if __name__ == "__main__":
