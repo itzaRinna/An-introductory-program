1ae39e3449e89fd8ae6a5ca5c69120c4f7702b53
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-08-31 00:45:56 -0400

    Made some improvements

---
+++ WELCOME_To_CyZero.py
@@ -0,0 +1,87 @@
+import sys
+from time import sleep
+
+ANIMATE_DURATION = 0.01
+SLOW_ANIMATE_DURATION = 0.09
+
+
+def txt_animate(text):
+    for letter in text:
+        print(letter, end="")
+        sys.stdout.flush()
+        sleep(ANIMATE_DURATION)
+
+
+def slow_txt_animate(text):
+    for letter in text:
+        print(letter, end="")
+        sys.stdout.flush()
+        sleep(SLOW_ANIMATE_DURATION)
+
+
+def main():
+    hello = '''
+      ██╗░░██╗███████╗██╗░░░░░██╗░░░░░░█████╗░██╗    
+      ██║░░██║██╔════╝██║░░░░░██║░░░░░██╔══██╗██║    
+      ███████║█████╗░░██║░░░░░██║░░░░░██║░░██║██║    
+      ██╔══██║██╔══╝░░██║░░░░░██║░░░░░██║░░██║╚═╝    
+      ██║░░██║███████╗███████╗███████╗╚█████╔╝██╗    
+      ╚═╝░░╚═╝╚══════╝╚══════╝╚══════╝░╚════╝░╚═╝    
+
+      ░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░
+      ░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗
+      ░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║
+      ░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║
+      ░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝
+      ░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░
+
+      ░█████╗░██╗░░░██╗███████╗███████╗██████╗░░█████╗░
+      ██╔══██╗╚██╗░██╔╝╚════██║██╔════╝██╔══██╗██╔══██╗
+      ██║░░╚═╝░╚████╔╝░░░███╔═╝█████╗░░██████╔╝██║░░██║
+      ██║░░██╗░░╚██╔╝░░██╔══╝░░██╔══╝░░██╔══██╗██║░░██║
+      ╚█████╔╝░░░██║░░░███████╗███████╗██║░░██║╚█████╔╝
+      ░╚════╝░░░░╚═╝░░░╚══════╝╚══════╝╚═╝░░╚═╝░╚════╝░
+    '''
+
+    txt_animate(hello)
+
+    transition = " "
+
+    print(transition)
+
+    proceed = input("Do you wish to continue ? :")
+    if proceed.lower() == 'yes':
+        print("Ello luv! We shall continue!")
+    elif proceed.lower() == 'no':
+        print("Ah alright darling, we shall meet in another day!")
+        exit()
+    else:
+        print("Hint:Use (Yes or No)")
+        exit()
+
+    print(transition)
+
+    slow_txt_animate("Greeting!\nAs a proper lad i am, i shall introduce me self.\nMy name is Cy William Bot the 3rd (royal)\n...but everyone calls me CyBot!\nI was designed by this mad lad named 'Tom'\nWhom i shall call 'the Boss'!\nDesigned to make simple questions...i was\nI seek to know you better!")
+
+    print(transition)
+
+    slow_txt_animate("So let's begin!")
+
+    print(transition)
+
+    qst_1 = input("What is your name ? M'lad ? ")
+    slow_txt_animate("Hello {}! Nice meeting ya!".format(qst_1))
+
+    print(transition)
+
+    slow_txt_animate(
+        "Great\nI have already known your name!\nWhat is your nickname?\n")
+    qst_2 = input("It's okay if you don't have one! ")
+    if qst_2.lower() == "no i don't have one" or "no i don't have one":
+        print("Then Goofy i shall call you!")
+    elif qst_2.lower() == "sorry i don't have a nickname" or "sorry i don't have a nickname":
+        print("Then Cutie i shall call you!")
+
+
+if __name__ == "__main__":
+    main()
