9b7eba08c5748addf9b7f75ad9ce32aae2aea94a
elloworldtom <111746841+elloworldtom@users.noreply.github.com>
2022-09-03 19:36:30 +0700

    Update WELCOME_To_CyZero.py

--- WELCOME_To_CyZero.py
+++ WELCOME_To_CyZero.py
@@ -233,3 +233,4 @@ if __name__ == "__main__":
     main()
 # Hey good boy, i would love to hear more about your changes to my previous program, since i cannot work this code out lmao!
 # Hello mommy, do the comments help?
+# Despise reading through your comments and updates to the program, i have yet to understand it fully
